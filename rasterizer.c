ca35b77a9bcbe3e1b4c23fd1aadbb97453775549
David Oberhollenzer <david.oberhollenzer@tele2.at>
2014-02-14 14:40:35 +0100

    Added custom rasterizer vertex structure
     - Does not have unneccessary parameters
     - Removed interpolation of unneccessary parameters
     - Fixed clipping
     - Depht already calculated in the rasterizer
     - Pixel drawing functions get pointer to constant value
    
    Signed-off-by: David Oberhollenzer <david.oberhollenzer@tele2.at>

--- rasterizer.c
+++ rasterizer.c
@@ -12,27 +12,36 @@
 
 
 
+typedef struct
+{
+    float z, w;
+    float s[MAX_TEXTURES], t[MAX_TEXTURES];
+    float r, g, b, a;
+}
+rs_vertex;
+
+
+
 /****************************************************************************
  *  Various implementations of the pixel draw function for differnet        *
  *  rasterizer configurations (blending, depth test, etc...)                *
  ****************************************************************************/
 
-typedef void (* pixel_fun )( vertex*, unsigned char*, float* );
+typedef void (* pixel_fun )( const rs_vertex*, unsigned char*, float* );
 
-static void pixel( vertex* v, unsigned char* color, float* depth )
+static void pixel( const rs_vertex* v, unsigned char* color, float* depth )
 {
     color[RED  ] = v->r * 255.0;
     color[GREEN] = v->g * 255.0;
     color[BLUE ] = v->b * 255.0;
     color[ALPHA] = v->a * 255.0;
 
-    depth[0] = (1.0f - v->z) * 0.5f;
+    depth[0] = v->z;
 }
 
-static void pixel_depth( vertex* v, unsigned char* color, float* depth )
+static void pixel_depth( const rs_vertex* v, unsigned char* color,
+                         float* depth )
 {
-    v->z = (1.0f - v->z) * 0.5f;
-
     if( v->z < depth[0] )
     {
         color[RED  ] = v->r * 255.0;
@@ -44,7 +53,8 @@ static void pixel_depth( vertex* v, unsigned char* color, float* depth )
     }
 }
 
-static void pixel_blend( vertex* v, unsigned char* color, float* depth )
+static void pixel_blend( const rs_vertex* v, unsigned char* color,
+                         float* depth )
 {
     unsigned int r, g, b, a, ia;
     a  = v->a * 255.0;
@@ -59,13 +69,13 @@ static void pixel_blend( vertex* v, unsigned char* color, float* depth )
     color[BLUE ] = ((color[BLUE ]*ia) >> 8) + b;
     color[ALPHA] = ((color[ALPHA]*ia) >> 8) + a;
 
-    depth[0] = (1.0f - v->z) * 0.5f;
+    depth[0] = v->z;
 }
 
-static void pixel_depth_blend( vertex* v, unsigned char* color, float* depth )
+static void pixel_depth_blend( const rs_vertex* v, unsigned char* color,
+                               float* depth )
 {
     unsigned int r, g, b, a, ia;
-    v->z = (1.0f - v->z) * 0.5f;
 
     if( v->z < depth[0] )
     {
@@ -122,24 +132,32 @@ void triangle_rasterize( const triangle* t, framebuffer* fb )
     int x, y, x0, x1, x2, y0, y1, y2, bl, br, bt, bb, i;
     unsigned char *scan, *ptr;
     unsigned char tex[4];
-    vertex A, B, C, v;
+    rs_vertex A, B, C, v;
 
     if( t->v0.w<=0.0 || t->v1.w<=0.0 || t->v2.w<=0.0 )
         return;
 
     /* prepare triangle vertices */
     A.w = 1.0/t->v0.w;
+    A.z = t->v0.z*A.w;
+    A.r = t->v0.r*A.w;
+    A.g = t->v0.g*A.w;
+    A.b = t->v0.b*A.w;
+    A.a = t->v0.a*A.w;
+
     B.w = 1.0/t->v1.w;
+    B.z = t->v1.z*B.w;
+    B.r = t->v1.r*B.w;
+    B.g = t->v1.g*B.w;
+    B.b = t->v1.b*B.w;
+    B.a = t->v1.a*B.w;
+
     C.w = 1.0/t->v2.w;
-
-    A.x=t->v0.x*A.w; A.y=t->v0.y*A.w; A.z=t->v0.z*A.w;
-    A.r=t->v0.r*A.w; A.g=t->v0.g*A.w; A.b=t->v0.b*A.w; A.a=t->v0.a*A.w;
-
-    B.x=t->v1.x*B.w; B.y=t->v1.y*B.w; B.z=t->v1.z*B.w;
-    B.r=t->v1.r*B.w; B.g=t->v1.g*B.w; B.b=t->v1.b*B.w; B.a=t->v1.a*B.w;
-
-    C.x=t->v2.x*C.w; C.y=t->v2.y*C.w; C.z=t->v2.z*C.w;
-    C.r=t->v2.r*C.w; C.g=t->v2.g*C.w; C.b=t->v2.b*C.w; C.a=t->v2.a*C.w;
+    C.z = t->v2.z*C.w;
+    C.r = t->v2.r*C.w;
+    C.g = t->v2.g*C.w;
+    C.b = t->v2.b*C.w;
+    C.a = t->v2.a*C.w;
 
     for( i=0; i<MAX_TEXTURES; ++i )
     {
@@ -149,12 +167,12 @@ void triangle_rasterize( const triangle* t, framebuffer* fb )
     }
 
     /* convert to raster coordinates */
-    x0 = (1.0f + A.x) * 0.5f * fb->width;
-    y0 = (1.0f - A.y) * 0.5f * fb->height;
-    x1 = (1.0f + B.x) * 0.5f * fb->width;
-    y1 = (1.0f - B.y) * 0.5f * fb->height;
-    x2 = (1.0f + C.x) * 0.5f * fb->width;
-    y2 = (1.0f - C.y) * 0.5f * fb->height;
+    x0 = (1.0f + t->v0.x*A.w) * 0.5f * fb->width;
+    y0 = (1.0f - t->v0.y*A.w) * 0.5f * fb->height;
+    x1 = (1.0f + t->v1.x*B.w) * 0.5f * fb->width;
+    y1 = (1.0f - t->v1.y*B.w) * 0.5f * fb->height;
+    x2 = (1.0f + t->v2.x*C.w) * 0.5f * fb->width;
+    y2 = (1.0f - t->v2.y*C.w) * 0.5f * fb->height;
 
     /* compute bounding rectangle */
     bl = MIN( x0, x1, x2 );
@@ -204,16 +222,13 @@ void triangle_rasterize( const triangle* t, framebuffer* fb )
                 continue;
 
             /* interpolate vertex coordinates and perform clipping */
-            v.x = A.x*a + B.x*b + C.x*c;
-            v.y = A.y*a + B.y*b + C.y*c;
             v.z = A.z*a + B.z*b + C.z*c;
             v.w = 1.0 / (A.w*a + B.w*b + C.w*c);
 
-            if( v.w<=0 || v.x>v.w || v.y>v.w || v.z>v.w )
+            if( v.w<=0 || v.z>1.0f || v.z<-1.0f )
                 continue;
 
-            if( v.x<-v.w || v.y<-v.w || v.z<-v.w )
-                continue;
+            v.z = (1.0f - v.z) * 0.5f;
 
             /* interpolate vertex attributes */
             v.r = (A.r*a + B.r*b + C.r*c) * v.w;
