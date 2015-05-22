244090f152dde121da3a13d4a37a8384f7ad4d10
David Oberhollenzer <david.oberhollenzer@tele2.at>
2015-05-23 00:20:35 +0200

    Add test program to demonstrate subpixel perfect rasterization
    Signed-off-by: David Oberhollenzer <david.oberhollenzer@tele2.at>

---
+++ test/subpixel.c
@@ -0,0 +1,45 @@
+#include "inputassembler.h"
+#include "framebuffer.h"
+#include "context.h"
+#include "window.h"
+
+
+
+int main( void )
+{
+    context ctx;
+    window* w;
+
+    if( !(w = window_create( 640, 480 )) )
+        return -1;
+
+    context_init( &ctx );
+    ctx.target = &w->fb;
+
+    context_set_viewport( &ctx, 0, 0, w->fb.width, w->fb.height );
+
+    while( window_handle_events( w ) )
+    {
+        framebuffer_clear( &w->fb, 0, 0, 0, 0xFF );
+
+        ia_begin( &ctx );
+
+        ia_color( &ctx, 1.0f, 1.0f, 1.0f, 1.0f );
+
+        ia_vertex( &ctx, -0.5f, -0.5f, 0.0f, 1.0f );
+        ia_vertex( &ctx, 0.5f, -0.5f, 0.0f, 1.0f );
+        ia_vertex( &ctx, -0.3f, 0.5f, 0.0f, 1.0f );
+
+        ia_vertex( &ctx, 0.3f, 0.5f, 0.0f, 1.0f );
+        ia_vertex( &ctx, 0.5f, -0.5f, 0.0f, 1.0f );
+        ia_vertex( &ctx, -0.3f, 0.5f, 0.0f, 1.0f );
+
+        ia_end( &ctx );
+
+        window_display_framebuffer( w );
+    }
+
+    window_destroy( w );
+    return 0;
+}
+
